91ed7fea19cf8871f3172f0505417136485809b2
Renu740 <Renu.Yadav@Ltts.com>
2021-08-16 16:50:54 +0530

    initial commit

---
+++ DependancyInjectionExample/MyDependency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependancyInjectionExample
+{
+    public class MyDependency:IMyDependency
+    {
+        public string WriteMessage(string message)
+        {
+            return "MyDependency.WriteMessage Message:" + message;
+        }
+    }
+}
---
+++ DependancyInjectionExample/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependancyInjectionExample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyDependency _dependency = new MyDependency();
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            string strMessage = _dependency.WriteMessage("IndexModel.OnGet created this message.");
+            return Content(strMessage);
+        }
+    }
+}
